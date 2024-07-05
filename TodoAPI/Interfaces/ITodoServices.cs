ad3a91090bd09ba2f788155f9d32029e53a5c4ef
Chargoy <achargoy@hotmail.com>
2024-07-04 23:28:57 -0500

    Implement the Service Layer and Service Interface

---
+++ TodoAPI/Interfaces/ITodoServices.cs
@@ -0,0 +1,12 @@
+﻿using TodoAPI.Models;
+
+namespace TodoAPI.Interfaces;
+
+public interface ITodoServices
+{
+    Task<IEnumerable<Todo>> GetAllAsync();
+    Task<Todo> GetByIdAsync(Guid id);
+    Task CreateTodoAsync(CreateTodoRequest request);
+    Task UpdateTodoAsync(Guid id,UpdateTodoRequest request);
+    Task DeleteTodoAsync(Guid id);
+}
---
+++ TodoAPI/Services/TodoServices.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using TodoAPI.AppDataContext;
+using TodoAPI.Interfaces;
+using TodoAPI.Models;
+
+namespace TodoAPI.Services;
+
+public class TodoServices : ITodoServices
+{
+    private readonly TodoDbContext _dbContext;
+    private readonly ILogger<TodoServices> _logger;
+    private readonly IMapper _mapper;
+    public TodoServices(TodoDbContext todoDbContext, ILogger<TodoServices> logger, IMapper mapper)
+    {
+        _dbContext = todoDbContext;
+        _logger = logger;
+        _mapper = mapper;
+    }
+    public Task CreateTodoAsync(CreateTodoRequest request)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteTodoAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Todo>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Todo> GetByIdAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateTodoAsync(Guid id, UpdateTodoRequest request)
+    {
+        throw new NotImplementedException();
+    }
+}
