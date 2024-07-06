a138594e0ab9d4056f5fa37028c42825114c3f8b
Chargoy <achargoy@hotmail.com>
2024-07-05 21:45:19 -0500

    Create the TodoController Class

---
+++ TodoAPI/Controllers/TodoController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TodoAPI.Interfaces;
+
+namespace TodoAPI.Controllers;
+[ApiController]
+[Route("api/[controller]")]
+public class TodoController : ControllerBase
+{
+    private readonly ITodoServices _todoService;
+    public TodoController(ITodoServices todoServices)
+    {
+        _todoService = todoServices;
+    }
+}
